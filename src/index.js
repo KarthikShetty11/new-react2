2b80bbdcb36496e8e398534f8d89e7c98d73b1e8
KarthikShetty11 <kvshetty27@gmail.com>
2022-04-07 20:15:25 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-ReactDOM.render(<h1>Helllo once again!!</h1>,document.getElementById('root'));
+ReactDOM.render(<h1>Hello once again!!!</h1>,document.getElementById('root'));
 
 
