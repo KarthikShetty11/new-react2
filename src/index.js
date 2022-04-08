3b23cbb499da658715caeecf8800271f65c159e3
KarthikShetty11 <kvshetty27@gmail.com>
2022-04-08 19:47:57 +0530

    bootstrap form added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-ReactDOM.render(<h1>Hello once again!!!</h1>,document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <nav>
+    <h1>KeeperApp</h1>
+    </nav>
+    
+    <form>
+    <label for="userNotes">Notes:</label><br/>
+    <input type="text" id="userNotes" name="username" value="" placeholder="Type here" />
+    <br />
+        <div class="input-group">
+         <div class="input-group-prepend">
+            <span class="input-group-text">Notes:</span>
+         </div>
+             <textarea class="form-control" aria-label="With textarea"></textarea>
+        </div>
+    
+</form>
 
-
+</div>
+  ,document.getElementById('root')
+);
