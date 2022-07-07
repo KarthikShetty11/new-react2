d2de1f7f9f8c04103f7ac194b721d3138071e9fe
KarthikShetty11 <kvshetty27@gmail.com>
2022-07-07 22:24:03 +0530

    Content component updated

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,17 +2,23 @@ import React from "react";
 
 function Content(){
     return(
-    <form>
-        <label for="userNotes">Notes:</label><br/>
-        <input type="text" id="userNotes" name="username" value="" placeholder="Type here" />
-    <br />
-        <div class="input-group">
-            <div class="input-group-prepend">
-            <span class="input-group-text">Notes:</span>
-            </div>
-            <textarea class="form-control" aria-label="With textarea"></textarea>
-        </div>
-    </form>
+    <div className="contentBox">
+      <h1>This is the Title</h1>
+      <p>Here goes the notes</p>  
+    </div>
+    
+
+    // <form>
+    //     <label for="userNotes">Notes:</label><br/>
+    //     <input type="text" id="userNotes" name="username" value="" placeholder="Type here" />
+    // <br />
+    //     <div class="input-group">
+    //         <div class="input-group-prepend">
+    //         <span class="input-group-text">Notes:</span>
+    //         </div>
+    //         <textarea class="form-control" aria-label="With textarea"></textarea>
+    //     </div>
+    // </form>
     )
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 function Footer(){
+
+    const currentYear = new Date().getFullYear();
 return(
     <footer>
-    <h4>thank you</h4>
+    <h4>Copyright {currentYear}</h4>
     </footer>
-)
+) 
 }
 
 export default Footer;
